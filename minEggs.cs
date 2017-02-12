4718362bd9081f428c4c3683da97d00908184027
hugh <hugh_fly@hotmail.com>
2017-02-13 08:21:01 +1300

    improved algorithm

---
+++ minEggs.cs
@@ -0,0 +1,16 @@
+public class minEggs
+{
+    public static void main()
+    {
+        int minEggs = 9;
+        while (true)
+        {
+            if (minEggs % 8 == 1 && minEggs % 7 == 0 && minEggs % )
+                break;
+            else
+                minEggs += 18;
+        }   
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
